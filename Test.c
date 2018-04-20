98993db9f85e37ebd7f443125fbf3c0b8b3e44bd
Andy R <xnorlogic@gmail.com>
2018-04-20 14:12:53 -0400

    Add stuff to Test.c

--- Test.c
+++ Test.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+void MyFunct();
 int main(){
   printf("hello Jenkins, good day.");
+  printf("\n");
+  MyFunct();
   retunr 0;
+}
+MyFunct{
+  printf("Print from the function");
 }
